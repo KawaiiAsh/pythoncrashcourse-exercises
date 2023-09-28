9fdc1fc1456baca6d1bf3c37c5970bfcc989915c
KaibutsuSama <kawaiiashsama@gmail.com>
2023-09-27 21:40:37 -0400

    Add: Solutions for Chapter 6

--- Chapter_06/alien.py
+++ Chapter_06/alien.py
@@ -5,3 +5,16 @@ print(alien_0['points'])
 
 new_points = alien_0['points']
 print("You just earned " + str(new_points) + " points!")
+
+alien_0 = {'color':'green','points':5}
+print(alien_0)
+
+alien_0['x_position'] = 0
+alien_0['y_position'] = 25
+print(alien_0)
+
+alien_0 = {}
+alien_0['color'] = 'green'
+alien_0['points'] = 5
+
+print(alien_0)
