9593a9b4ff2384275149b063153e0164b1920e63
KaibutsuSama <kawaiiashsama@gmail.com>
2023-10-11 01:58:30 -0400

    Add: Solutions for Chapter 15

--- Chapter_15/mpl_squares.py
+++ Chapter_15/mpl_squares.py
@@ -1,15 +1,15 @@
 import matplotlib.pyplot as plt
 
-input_value = [1, 2, 3, 4, 5]
+input_values = [1, 2, 3, 4, 5]
 squares = [1, 4, 9, 16, 25]
-plt.plot(input_value, squares, linewidth=5)
+plt.plot(input_values, squares, linewidth=5)
 
-# Set title and label
-plt.title("Squares numbers", fontsize=24)
+# Set chart title and label axes.
+plt.title("Square Numbers", fontsize=24)
 plt.xlabel("Value", fontsize=14)
 plt.ylabel("Square of Value", fontsize=14)
 
-# Set tick size
+# Set size of tick labels.
 plt.tick_params(axis='both', labelsize=14)
 
 plt.show()
